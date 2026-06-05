9aefba13b5dbcddd86c7899a5bd52efcfdd6a8ef
Ethan Jones <ethanjones314@gmail.com>
2026-06-05 12:32:04 -0500

    Compute sphere normals + normal vis

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,22 @@ use crate::vec3::Vec3;
 
 use image::{Rgb, RgbImage};
 
+/// Converts a vector representing a color into an RGB value.
+///
+/// Assumes that the provided vector's components are each in the interval
+/// [0.0, 1.0]. If not, the resulting color's components will still be clamped
+/// to within [0, 255], but it may result in odd visuals.
+fn to_color(v: Vec3) -> Rgb<u8> {
+    Rgb(v.0.map(|component| (component * 255.0) as u8))
+}
+
 /// Determines whether the given ray hits a sphere with a given center and
 /// radius.
-fn hits_sphere(center: Vec3, radius: f64, ray: &Ray) -> bool {
+///
+/// If the given ray hits the sphere, returns `Some` containing the time
+/// at which the ray intersects the sphere. Otherwise, if the ray does not hit
+/// the sphere, returns `None`.
+fn hits_sphere(center: Vec3, radius: f64, ray: &Ray) -> Option<f64> {
     // A sphere is a set of vectors P that are all a distance of r away from
     // the center C. We can express this using a dot product:
     //      (C - P) . (C - P) = r^2
@@ -32,28 +45,34 @@ fn hits_sphere(center: Vec3, radius: f64, ray: &Ray) -> bool {
     let b = -2.0 * ray.direction.dot(&qc);
     let c = qc.dot(&qc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant >= 0.0
-    // TODO: This implementation allows for negative values of t, so rays can
-    // "hit" a sphere placed behind the camera. This isn't correct, but it
-    // works for an initial implementation.
+    if discriminant < 0.0 {
+        None
+    } else {
+        Some((-b - discriminant.sqrt()) / (2.0 * a))
+    }
 }
 
 /// Returns the pixel color associated with a given ray.
 fn ray_color(ray: &Ray) -> Rgb<u8> {
-    // Test for object collisions:
-    if hits_sphere(Vec3([0.0, 0.0, -1.0]), 0.5, ray) {
-        return Rgb([240, 21, 56]);
-    }
+    const SPHERE_CENTER: Vec3 = Vec3([0.0, 0.0, -1.0]);
+    const RADIUS: f64 = 0.5;
 
+    // Test for object collisions:
+    if let Some(t) = hits_sphere(SPHERE_CENTER, RADIUS, ray) {
+        // Compute the unit normal vector at the intersection point:
+        let normal = (ray.at_time(t) - SPHERE_CENTER).normalized();
+        let normal_scaled = 0.5 * (normal + Vec3([1.0, 1.0, 1.0]));
+        let normal_color = to_color(normal_scaled);
+        return normal_color;
+    }
     // If the ray doesn't hit any objects, draw a gradient background.
     // Lerp from white to blue as the y component of the ray increases:
     let unit_dir = ray.direction.normalized();
     const WHITE: Vec3 = Vec3([1.0, 1.0, 1.0]);
     const BLUE: Vec3 = Vec3([0.5, 0.7, 1.0]);
-    let blend = 0.5 * unit_dir.y() + 1.0;
-    let color = (1.0 - blend) * WHITE + blend * BLUE;
-    // Convert color scale from 0.0-1.0 to 0-255.
-    Rgb(color.0.map(|component| (255.99 * component).round() as u8))
+    let blend = 0.5 * (unit_dir.y() + 1.0);
+    let bg_color_vector = (1.0 - blend) * WHITE + blend * BLUE;
+    to_color(bg_color_vector)
 }
 
 fn main() {
@@ -65,12 +84,11 @@ fn main() {
 
     // Viewport scaling.
     const VIEWPORT_HEIGHT: f64 = 2.0;
-    const VIEWPORT_WIDTH: f64 = VIEWPORT_HEIGHT * IMAGE_WIDTH as f64 / IMAGE_HEIGHT as f64;
+    const VIEWPORT_WIDTH: f64 = VIEWPORT_HEIGHT * (IMAGE_WIDTH as f64 / IMAGE_HEIGHT as f64);
 
     // Camera settings.
     const FOCAL_LENGTH: f64 = 1.0;
     const CAMERA_CENTER: Vec3 = Vec3::new();
-
 
     // Spacial coordinates: +X is right, +Y is up, +Z points out of the screen.
     // Viewport coordinates: +U is right, +V is down.
@@ -85,17 +103,21 @@ fn main() {
     // Compute the origin (top-left) of the viewport and the center of the
     // first pixel.
     const VIEWPORT_DEPTH: Vec3 = Vec3([0.0, 0.0, -FOCAL_LENGTH]);
-    let viewport_origin = CAMERA_CENTER + VIEWPORT_DEPTH - VIEWPORT_U / 2.0 - VIEWPORT_V / 2.0;
+    let viewport_origin = CAMERA_CENTER + VIEWPORT_DEPTH - (0.5 * VIEWPORT_U) - (0.5 * VIEWPORT_V);
     let first_pixel = viewport_origin + 0.5 * (step_u + step_v);
 
     let mut img = RgbImage::new(IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for row in 0..IMAGE_HEIGHT {
         for col in 0..IMAGE_WIDTH {
+            // Construct a ray travelling from the camera through the center
+            // of the current pixel:
             let pixel_center = first_pixel + (step_u * col as f64) + (step_v * row as f64);
             let ray_direction = pixel_center - CAMERA_CENTER;
             let ray = Ray::new(pixel_center, ray_direction);
-            img.put_pixel(col, row, ray_color(&ray));
+            // Compute the ray's color and write it to the image buffer:
+            let px_color = ray_color(&ray);
+            img.put_pixel(col, row, px_color);
         }
     }
 
